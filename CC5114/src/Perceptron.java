97349cdd0ab5d95a9c3273db805995bbc57f3163
Patricio Merino <patitomv@gmail.com>
2017-08-06 21:38:11 -0400

    Adds getters and setters

--- CC5114/src/Perceptron.java
+++ CC5114/src/Perceptron.java
@@ -7,5 +7,39 @@
  *
  */
 public class Perceptron {
+	private double[] weights;
+	private double bias;
+	
+	public Perceptron(double[] weights, double bias) {
+		this.weights = weights;
+		this.bias = bias;
+	}
 
+	/**
+	 * @return the weights
+	 */
+	public double[] getWeights() {
+		return weights;
+	}
+
+	/**
+	 * @param weights the weights to set
+	 */
+	public void setWeights(double[] weights) {
+		this.weights = weights;
+	}
+
+	/**
+	 * @return the bias
+	 */
+	public double getBias() {
+		return bias;
+	}
+
+	/**
+	 * @param bias the bias to set
+	 */
+	public void setBias(double bias) {
+		this.bias = bias;
+	}
 }
